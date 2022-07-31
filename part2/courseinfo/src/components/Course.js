c787b128aa7b18d16b5b55bf2119190f9863c44c
Julián Maestri <serpi90@gmail.com>
2022-07-31 18:24:32 -0300

    :recycle: move course to a separate module

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,15 @@
+const Course = ({ course }) => (<div>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+</div>)
+
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Part = ({ part }) => <p> {part.name} {part.exercises} </p>;
+
+const Content = ({ parts }) => parts.map(part => <Part part={part} key={part.id} />);
+
+const Total = (props) => <p><strong>Total of {props.parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</strong></p>;
+
+export default Course;
