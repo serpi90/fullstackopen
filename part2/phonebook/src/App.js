33de97c3c196243f88b867530c5c3d29a2b7bd6b
Julián Maestri <serpi90@gmail.com>
2022-07-31 19:43:04 -0300

    :sparkles: list persons

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { id: 1, name: 'Arto Hellas' }
   ])
   const [newName, setNewName] = useState('')
 
@@ -18,7 +18,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(p => <p key={p.id}>{p.name}</p>)}
     </div>
   )
 }
