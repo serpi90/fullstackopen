ef3ac1fc905b276b88ec9e3a3fa94c35b18f5aff
Julián Maestri <serpi90@gmail.com>
2022-07-31 13:42:56 -0300

    :recycle: extract statistics into a component

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,28 @@
 import { useState } from 'react'
 
+const Statistics = ({ good, neutral, bad }) => {
+  const total = () => good + neutral + bad;
+  const average = () => ((good - bad) / total()).toFixed(2);
+  const positive = () => ((good) / total() * 100).toFixed(0) + '%';
+
+  return (
+    <dl>
+      <dt>Good</dt>
+      <dd>{good}</dd>
+      <dt>Neutral</dt>
+      <dd>{neutral}</dd>
+      <dt>Bad</dt>
+      <dd>{bad}</dd>
+      <dt>All</dt>
+      <dd>{total()}</dd>
+      <dt>Average</dt>
+      <dd>{average()}</dd>
+      <dt>Positive</dt>
+      <dd>{positive()}</dd>
+    </dl>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -10,10 +33,6 @@ const App = () => {
   const increaseNeutral = () => setNeutral(neutral + 1);
   const increaseBad = () => setBad(bad + 1);
 
-  const total = () => good + neutral + bad;
-  const average = () => ((good - bad) / total()).toFixed(2);
-  const positive = () => ((good) / total() * 100).toFixed(0) + '%';
-
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -22,20 +41,7 @@ const App = () => {
       <button onClick={increaseBad}>Bad</button>
       <section>
         <h2>Statistics</h2>
-        <dl>
-          <dt>Good</dt>
-          <dd>{good}</dd>
-          <dt>Neutral</dt>
-          <dd>{neutral}</dd>
-          <dt>Bad</dt>
-          <dd>{bad}</dd>
-          <dt>All</dt>
-          <dd>{total()}</dd>
-          <dt>Average</dt>
-          <dd>{average()}</dd>
-          <dt>Positive</dt>
-          <dd>{positive()}</dd>
-        </dl>
+        <Statistics good={good} neutral={neutral} bad={bad} />
       </section>
     </div>
   )
