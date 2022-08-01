d1416f611d0c0052c0d0b441aa4e97907e6c4272
Julián Maestri <serpi90@gmail.com>
2022-08-01 00:47:30 -0300

    :sparkles: add search by name

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,9 +3,13 @@ import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
+  const [search, setSearch] = useState('')
 
   const addNewPerson = event => {
     event.preventDefault();
@@ -25,6 +29,12 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        <div>
+          filter shown with <input value={search} onChange={event => setSearch(event.target.value)} />
+        </div>
+      </div>
+      <h2>add a new</h2>
       <form>
         <div>
           name: <input value={newName} onChange={event => setNewName(event.target.value)} />
@@ -37,7 +47,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(p => <p key={p.id}>{p.name}</p>)}
+      {persons
+        .filter(p => p.name.toLowerCase().includes(search.toLowerCase()))
+        .map(p => <p key={p.id}>{p.name}</p>)}
     </div>
   )
 }
