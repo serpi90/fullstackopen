73a94b5607670931edd8faaf97572200cd95afb2
Julián Maestri <serpi90@gmail.com>
2022-08-01 02:39:07 -0300

    :sparkles: add country information on exact match

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,7 +1,24 @@
+const Country = ({ country }) => {
+  return <>
+    <h2>{country.name.common}</h2>
+    <p>Capital: {country.capital.join(', ')}</p>
+    <p>Area: {country.area} km²</p>
+    <h3>Languages:</h3>
+    <ul>
+      {Object
+        .entries(country.languages)
+        .map(([key, lang]) => <li key={key}>{lang}</li>)}
+    </ul>
+    <img src={country.flags.png} alt="flag" />
+  </>;
+}
+
 const Countries = ({ countries, search }) => {
   const filtered = countries.filter(p => p.name.common.toLowerCase().includes(search.toLowerCase()));
   if (filtered.length > 10) {
     return <p>Too many matches, specify another filter</p>
+  } else if (filtered.length === 1) {
+    return <Country country={filtered[0]} />
   }
   return filtered.map(p => <p key={p.cca3}>{p.name.common}</p>);
 };
