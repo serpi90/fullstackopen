cecb9e5307197ac01d0fad52f1f2a6407ac16d62
Julián Maestri <serpi90@gmail.com>
2022-08-04 01:15:36 -0300

    :recycle: cleanup PersonForm

--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -5,26 +5,34 @@ const PersonForm = ({ persons, setPersons }) => {
   const [newName, setNewName] = useState('');
   const [newPhone, setNewPhone] = useState('');
 
-  const addNewPerson = event => {
-    event.preventDefault();
-    const person = persons.find(p => p.name === newName);
-    if (person) {
-      if (window.confirm(`${person.name} is already added to phonebook, replace the old number with a new one?`)) {
-        personsService.update(person.id, { ...person, number: newPhone })
-          .then(updatedPerson => {
-            setPersons(persons.map(p => p.id === person.id ? updatedPerson : p));
-            setNewName('');
-            setNewPhone('');
-          });
-      }
-    } else {
-      personsService
-        .create({ name: newName, phone: newPhone })
-        .then(newPerson => {
-          setPersons(persons.concat(newPerson));
+  const addPerson = (person) => {
+    personsService
+      .create(person)
+      .then(newPerson => {
+        setPersons(persons.concat(newPerson));
+        setNewName('');
+        setNewPhone('');
+      });
+  };
+
+  const updatePerson = (person) => {
+    if (window.confirm(`${person.name} is already added to phonebook, replace the old number with a new one?`)) {
+      personsService.update(person.id, { ...person, number: newPhone })
+        .then(updatedPerson => {
+          setPersons(persons.map(p => p.id === person.id ? updatedPerson : p));
           setNewName('');
           setNewPhone('');
         });
+    }
+  };
+
+  const upsertPerson = event => {
+    event.preventDefault();
+    const person = persons.find(p => p.name === newName);
+    if (person) {
+      updatePerson(person);
+    } else {
+      addPerson({ name: newName, number: newPhone });
     }
   };
 
@@ -37,7 +45,7 @@ const PersonForm = ({ persons, setPersons }) => {
         number: <input value={newPhone} onChange={event => setNewPhone(event.target.value)} />
       </div>
       <div>
-        <button type="submit" onClick={addNewPerson}>add</button>
+        <button type="submit" onClick={upsertPerson}>add/update</button>
       </div>
     </form>
   );
